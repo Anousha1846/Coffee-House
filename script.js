714e309174bed7d886470c5ca2ce6c667695f602
Anousha1846 <125111686+Anousha1846@users.noreply.github.com>
2024-01-07 04:10:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// var searchForm = document.getElementById("searchForm");
+var searchFormID = document.getElementById('searchFormID');
+
+
+function openSearch() {
+   searchForm.classList.add("hide")
+}
+
+// menu start
+var seeMoreINmENU = document.getElementById('seeMoreINmENU');
+var moreMenu = document.getElementById('seeMoreMenu');
+
+function moreMenu(){
+
+}
+// menu end
+
+//products start 
+function seeMoreProducts(){
+var moreProducts = document.getElementById("seeMoreProducts");
+moreProducts.classList.toggle('hide')
+}
+// products end
+
+// review start
+ function moreReviews(){
+  var moreReviews = document.getElementById('moreReviews')
+  moreReviews.classList.toggle('hide')
+ }
+// review end
